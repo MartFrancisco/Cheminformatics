d5e48fa91d70efb5879c74f04499841badeac62d
fmartins <francisco.qui.martins@gmail.com>
2026-02-08 11:08:18 -0600

    updating_setup

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
-# Read the README for the long description
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="chemspace-explorer",
+    name="cheminformatics",
     version="0.1.0",
     author="Francisco Martins",
     author_email="francisco.qui.martins@gmail.com",
     description="Diverse amide library design pipeline for cheminformatics",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/chemspace-explorer",
+    url="https://github.com/MartFrancisco/cheminformatics",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -34,8 +33,4 @@ setup(
         "matplotlib>=3.6.0",
         "mols2grid>=2.0.0",
     ],
-    extras_require={
-        "dev": ["pytest>=7.0", "black", "flake8"],
-        "notebooks": ["jupyter>=1.0.0", "ipykernel>=6.0.0"],
-    },
 )
